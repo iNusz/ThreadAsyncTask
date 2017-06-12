0a2fb27ef8fcf1a17dfae79b27079a434c971271
SeungHo Shin <94inusz@gmail.com>
2017-06-13 02:50:39 +0900

    add AsyncTask

--- README.md
+++ README.md
@@ -39,22 +39,29 @@ Handler handler = new Handler() {
 ```
 
 
+<br/>
+
+
+
 ## ProgressDialog
 
 
+
 ProgressDialog를 만들어 보자
+
+
 
 <br/>
 
 
 
 
-###### 프로그래스 다이얼로그를 정의한다.
+#### 프로그래스 다이얼로그를 정의한다.
 
 
 
 
-###### setTitle , setMessage , setProgressStyle 는 필수요소이다.
+setTitle , setMessage , setProgressStyle 는 필수요소이다.
 
 
 
@@ -78,7 +85,9 @@ ProgressDialog를 만들어 보자
 
 
 
-###### run()에서 show()로 창을 호출해준다
+#### run()
+
+run()에서 show()로 창을 호출해준다
 
 
 
@@ -90,7 +99,12 @@ private void run() {
 
 
 
-###### setDone() --> 프로그래스가 끝나는부분에서 dismiss();로 창을 해제한다
+#### setDone()
+
+
+
+프로그래스가 끝나는부분에서 dismiss();로 창을 해제한다
+
 
 
 
@@ -99,3 +113,168 @@ private void setDone() {
       progress.dismiss();
 }
 ```
+
+
+
+<br/>
+
+
+
+
+## AsyncTask
+
+
+
+
+AsyncTask는 Task가 한정적일때(1~10초 에서 끝난다거나), 10가지일만 처리하고 끝나거나, 루틴이 정해져있을때 사용한다(채팅앱x)
+
+
+
+주로 네트워크를 통해서 데이터를 가져오거나 목록을 뿌려줄때 사용한다.
+
+
+
+
+<br/>
+
+
+#### 일반적인 형태
+
+
+execute는 Thread에서 start과 같은 역활을 한다.
+
+execute에서는 메인쓰레드에서 -> 서브쓰레드 (doInBackground) 로 값을 넘겨준다. 서로 값을 넘겨줄수 있다
+
+
+이때 execute는 doInBackground 를 실행시켜주는데(그전에 onPreExecute가 실행) 괄호 안에 값을 ,로 구분해서 입력하면 배열처럼 순차적으로 출력이 되어서 값을 넘겨줄 수 있다
+
+
+```java
+private void runAsync() {
+
+    new AsyncTask<String, Integer, Float>() {
+            // 제네릭 타입   1 - doInBackground 의 인자
+            //            2 - onProgressUpdate 의 인자
+            //            3 - doInBackground 의 리턴타입
+
+
+    }.execute("안녕", "하세요");
+}
+```
+
+
+
+
+#### onPreExecute
+
+
+
+doInBackground 가 호출되기 전에 먼저 호출된다. 메인쓰레드에서 실행된다.
+
+
+
+
+```java
+          @Override
+          protected void onPreExecute() {
+              progress = new ProgressDialog(MainActivity.this);
+              progress.setTitle("진행중...");
+              progress.setMessage("진행중 표시되는 메세지입니다");
+              progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+              progress.show();
+          }
+```
+
+
+
+
+#### doInBackground
+
+
+
+
+Thread에서 run과 같은 역활을 하는데 데이터를 처리하는 역활을 해준다. 서브쓰레드에서 실행된다.
+
+
+
+여기서 return값을 넘겨줄 경우에 onPostExecute에 값이 넘어가면서 실행이 되는데, 이를 활용하는 방법으로는
+
+
+
+
+날씨를 예를 들경우 위의 서브쓰레드(onPreExecute)에서 "맑음"이라는 데이터를 가져온다음에 return으로 "맑음"의 결과값을 onPostExecute(메인)으로 넘겨줄수 있다.
+
+
+
+
+```java
+            @Override
+            // String ... params  <-- ...은 크기를 정의되지않은 params 배열이라고 생각하면 됨
+            protected Float doInBackground(String... params) {
+                // 10초 후에 Done 이 뜨게끔 한다 .
+                try {
+                    for(int i =0; i<10; i++) {
+                        // onProgressUpdate 를 주기적으로 업데이트 해주는 메소드
+                        publishProgress(i*10);
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return 1004.4f; // 이때 onPostExecute 가 실행된다  , 여기에 넘길 값을 쓰면 된다
+            }
+```
+
+
+
+#### onPostExecute
+
+
+
+doInBackground 가 처리되고 나서 호출된다. 메인쓰레드에서 실행된다.
+
+
+
+이 메소드는 위의 서브스레드(doInBackground)의 처리가 끝나고 나서 MainThread로 값을 넘겨줄수 있다는 얘기이다.
+
+
+
+
+```java
+            @Override
+            protected void onPostExecute(Float aVoid) {
+                // 결과값을 메인 UI에 세팅하는 로직을 여기에 작성한다
+                setDone();
+                progress.dismiss();
+            }
+```
+
+
+
+#### onProgressUpdate
+
+
+
+주기적으로 doInBackground 에서 호출이 가능한 메소드이다. 진행률을 나타낼수 있다.
+
+
+
+```java
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                progress.setMessage("진행율 = " +values[0]+ "%");
+            }
+```
+
+
+
+
+
+## Android Emulator
+
+
+handler를 이용한 앱 실행, ProgressDialog 활용 , AsyncTask를 이용한 ProgressDialog 활용
+
+
+
+![Async.jpg]()
--- app/src/main/java/com/seunghoshin/android/threadasynctask_2/MainActivity.java
+++ app/src/main/java/com/seunghoshin/android/threadasynctask_2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.seunghoshin.android.threadasynctask_2;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int SET_DONE = 1;
     TextView textView;
-    ProgressDialog progress;
+
 
     // thread에서 호출하기 위한 handler
     Handler handler = new Handler() {
@@ -38,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                run();
+                runAsync();
             }
         });
         // 화면에 진행상태를 표시
@@ -47,34 +48,98 @@ public class MainActivity extends AppCompatActivity {
         // this에는 캐스팅을 통해서 context에서 하위로 다운캐스팅이 된다 . 최상위 context를 넘겨주면 하위로 다시 캐스팅이 되지 않는다(context 리소스만 가지고있다)
         // 다이얼로그는 Theme를 쓰는데 context뿐만이 아니라 activity에 style요소를 가지고 있기 때문에 this를 써야한다
 
-        progress = new ProgressDialog(this);
-        progress.setTitle("진행중...");
-        progress.setMessage("진행중 표시되는 메세지입니다");
-        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+
     }
 
 
     private void setDone() {
+        // todo 이렇게 되면 setDone메소드가 구지 있어야 되는 생각이 .. 차라리 onPostExecute에 넣어주면 어떨까..
         textView.setText("Done!!!");
         // 프로그래스 창을 해제
-        progress.dismiss();
+        // progress.dismiss();
     }
 
-    private void run() {
-        // 프로그래스 창을 호출
-        progress.show();
-        CustomThread thread = new CustomThread(handler);
-        thread.start();
+    // 사용할때는 Task가 한정적일때 (1~10초)에서 끝난다거나 , 10가지일만 처리하고 끝난다거나 , 루틴이 정해져있을때 쓴다. (채팅같은건 당연히안씀)
+    // 주로 네트워크 통해서 데이터 가져오거나 목록을 뿌려줄때 사용한다
+    private void runAsync() {
+
+        new AsyncTask<String, Integer, Float>() {
+
+            // 제네릭 타입   1 - doInBackground 의 인자
+            //            2 - onProgressUpdate 의 인자
+            //            3 - doInBackground 의 리턴타입
+
+            ProgressDialog progress;
+
+
+            // doInBackground 가 호출되기 전에 먼저 호출된다   , 메인쓰레드에서 실행
+            @Override
+            protected void onPreExecute() {
+                progress = new ProgressDialog(MainActivity.this);
+                progress.setTitle("진행중...");
+                progress.setMessage("진행중 표시되는 메세지입니다");
+                progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                progress.show();
+            }
+
+            // 서브쓰레드에서 실행
+            @Override
+            // String ... params  <-- ...은 크기를 정의되지않은 params 배열이라고 생각하면 됨
+            protected Float doInBackground(String... params) { // Thread에서 run과 같은 역활을 하는데 데이터를 처리하는 역활을 해줌 ...
+                // 10초 후에 Done 이 뜨게끔 한다 .
+                try {
+                    for(int i =0; i<10; i++) {
+                        // onProgressUpdate 를 주기적으로 업데이트 해준다.
+                        publishProgress(i*10);
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                // 날씨를 예를 들경우 위의 서브쓰레드에서 맑음이라는 데이터를 가져온다음에 return으로 맑음의 결과값을 onPostExecute(메인)으로 넘겨줄수 있다
+                return 1004.4f; // 이때 onPostExecute 가 실행된다  , 여기에 넘길 값을 쓰면 된다
+            }
+
+
+            // doInBackground 가 처리되고 나서 호출된다   , 메인쓰레드에서 실행
+            @Override
+            protected void onPostExecute(Float aVoid) {  // 위의 서브스레드의 처리가 끝나고 나서 MainThread로 값을 넘겨줄수 있다는 얘기임
+                // 결과값을 메인 UI에 세팅하는 로직을 여기에 작성한다
+                setDone();
+                progress.dismiss();
+            }
+
+            // 주기적으로 doInBackground 에서 호출이 가능한 함수   , 진행률을 나타낼수 있다   todo onProgressUpdate = 메인쓰레드 ? 서브쓰레드 ?
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                // todo values[0]의 동작원리 , values를 넣으면 왜 주소값이 나오는지 ..
+                progress.setMessage("진행율 = " +values[0]+ "%");
+            }
+
+
+            //Thread에서 start과 같은 역활을 한다   , execute에서는 메인쓰레드에서 -> 서브쓰레드 (doInBackground) 로 값을 넘겨준다 , 서로값을넘겨줄수 있다
+        }.execute("안녕", "하세요");  // doInBackground 를 실행(그전에 pre가실행) , 이렇게 출력해주면 순서대로 배열처럼 꺼내 쓸 수 있다(같은타입만가능)
     }
+
+
+//    일반 쓰레드
+
+//    private void runThread() {
+//        // 프로그래스 창을 호출
+//        progress.show();
+//        CustomThread thread = new CustomThread(handler);
+//        thread.start();
+//    }
 }
 
 
 class CustomThread extends Thread {
     Handler handler;
 
-    public CustomThread(Handler handler){
+    public CustomThread(Handler handler) {
         this.handler = handler;
     }
+
     @Override
     public void run() {
         // 10초 후에 Done 이 뜨게끔 한다 .
