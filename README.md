e6aec15439590a5efdc08534221d8cac1d0a9602
SeungHo Shin <94inusz@gmail.com>
2017-06-13 01:11:11 +0900

    add Progress Dialog

--- README.md
+++ README.md
@@ -37,3 +37,65 @@ Handler handler = new Handler() {
         }
     }
 ```
+
+
+## ProgressDialog
+
+
+ProgressDialog를 만들어 보자
+
+<br/>
+
+
+
+
+###### 프로그래스 다이얼로그를 정의한다.
+
+
+
+
+###### setTitle , setMessage , setProgressStyle 는 필수요소이다.
+
+
+
+
+```java
+@Override
+            public void onClick(View v) {
+                run();
+            }
+        });
+        // 화면에 진행상태를 표시
+        // 여기서 this 대신에 getBaseContext()를 못쓰는 이유는 context에는 context만 들어가있다
+        // this에는 캐스팅을 통해서 context에서 하위로 다운캐스팅이 된다 . 최상위 context를 넘겨주면 하위로 다시 캐스팅이 되지 않는다(context 리소스만 가지고있다)
+        // 다이얼로그는 Theme를 쓰는데 context뿐만이 아니라 activity에 style요소를 가지고 있기 때문에 this를 써야한다
+        progress = new ProgressDialog(this);
+        progress.setTitle("진행중...");
+        progress.setMessage("진행중 표시되는 메세지입니다");
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+    }
+```
+
+
+
+###### run()에서 show()로 창을 호출해준다
+
+
+
+```java
+private void run() {
+    progress.show();
+}
+```
+
+
+
+###### setDone() --> 프로그래스가 끝나는부분에서 dismiss();로 창을 해제한다
+
+
+
+```java
+private void setDone() {
+      progress.dismiss();
+}
+```
--- app/src/main/java/com/seunghoshin/android/threadasynctask_2/MainActivity.java
+++ app/src/main/java/com/seunghoshin/android/threadasynctask_2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.seunghoshin.android.threadasynctask_2;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int SET_DONE = 1;
     TextView textView;
+    ProgressDialog progress;
 
     // thread에서 호출하기 위한 handler
     Handler handler = new Handler() {
@@ -39,13 +41,28 @@ public class MainActivity extends AppCompatActivity {
                 run();
             }
         });
+        // 화면에 진행상태를 표시
+        // 프로그래스 다이얼로그 정의
+        // 여기서 this 대신에 getBaseContext()를 못쓰는 이유는 context에는 context만 들어가있다
+        // this에는 캐스팅을 통해서 context에서 하위로 다운캐스팅이 된다 . 최상위 context를 넘겨주면 하위로 다시 캐스팅이 되지 않는다(context 리소스만 가지고있다)
+        // 다이얼로그는 Theme를 쓰는데 context뿐만이 아니라 activity에 style요소를 가지고 있기 때문에 this를 써야한다
+
+        progress = new ProgressDialog(this);
+        progress.setTitle("진행중...");
+        progress.setMessage("진행중 표시되는 메세지입니다");
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
     }
+
 
     private void setDone() {
         textView.setText("Done!!!");
+        // 프로그래스 창을 해제
+        progress.dismiss();
     }
 
     private void run() {
+        // 프로그래스 창을 호출
+        progress.show();
         CustomThread thread = new CustomThread(handler);
         thread.start();
     }
